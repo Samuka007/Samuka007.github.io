62928573e80b9939e1728e35f3fa3247bd281c60
Samuel Dai <samuka007@dragonos.org>
2025-03-28 22:58:42 +0800

    Update sidebar.ts

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -6,6 +6,7 @@ export const sidebar = {
         { text: '继续未完成的旅途', link: '/blog/continue' },
         { text: '☁️ 云服务', link: '/blog/cloud' },
         { text: '2025冬训-Week3', link: '/blog/2025-winter-train-week3' },
+        { text: 'Server Misc', link: '/blog/server-config' },
       ]
     }
   ],
